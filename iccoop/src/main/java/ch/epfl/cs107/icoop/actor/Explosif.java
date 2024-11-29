e356ebe33ec97bcb35119dbd70d84ca6eb9aaf3e
Blaazen <blqzen@gmail.com>
2024-11-29 15:59:14 +0100

    avant le pull d'axel

--- iccoop/src/main/java/ch/epfl/cs107/icoop/actor/Explosif.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/Explosif.java
@@ -1,5 +1,6 @@
 package ch.epfl.cs107.icoop.actor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.cs107.icoop.handler.ICoopInteractionVisitor;
@@ -13,7 +14,6 @@ import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.math.Orientation;
 import ch.epfl.cs107.play.window.Canvas;
 
-<<<<<<< HEAD
 public class Explosif extends AreaEntity implements Interactor{
 
     private boolean isActivated;
@@ -21,6 +21,8 @@ public class Explosif extends AreaEntity implements Interactor{
     private boolean isExploding;
     private final static int ANIMATION_DURATION = 7;
     private ExplosifInteractionHandler interactionHandler = new ExplosifInteractionHandler();
+    private Animation normalAnimation;
+    private Animation explosionAnimation;
 
 
     // Constructeur ici
@@ -29,6 +31,7 @@ public class Explosif extends AreaEntity implements Interactor{
         this.isActivated = false;
         this.isExploding = false;
         this.counter = counter;
+        
     }
 
     @Override
@@ -79,8 +82,12 @@ public class Explosif extends AreaEntity implements Interactor{
      */
     @Override
     public List<DiscreteCoordinates> getFieldOfViewCells(){
-
-    }
+        List<DiscreteCoordinates> neighbourCells = new ArrayList<>();
+        for (Orientation orientation : Orientation.values()) {
+            neighbourCells.add(getCurrentMainCellCoordinates().jump(orientation.toVector()));
+        }
+        return neighbourCells;
+        }
 
 
     /**@return (boolean): true if this require cell interaction */
@@ -120,13 +127,13 @@ public class Explosif extends AreaEntity implements Interactor{
     /**@return (boolean): true if this is able to have cell interactions*/
     @Override
     public boolean isCellInteractable(){
-
+        return(!isExploding && isActivated);
     }
 
     /**@return (boolean): true if this is able to have view interactions*/
     @Override
     public boolean isViewInteractable(){
-
+        return (!isExploding);
     }
 
     /** Call directly the interaction on this if accepted
@@ -147,108 +154,3 @@ public class Explosif extends AreaEntity implements Interactor{
         }
     }
 }
-=======
-//public class Explosif extends AreaEntity implements Interactor {
-//
-//    private boolean isActivated;
-//    private int counter;
-//    private boolean isExploding;
-//
-//    // Constructeur ici
-//    public Explosif(Area area, Orientation orientation, DiscreteCoordinates position, int counter){
-//        super(area, orientation, position);
-//        this.isActivated = false;
-//        this.isExploding = false;
-//        this.counter = counter;
-//    }
-//
-//    public void update(float deltaTime){
-//        super.update(deltaTime);
-//        if (isActivated){
-//        counter -= 1;
-//        }
-//
-//        if(counter <= 0){
-//            isExploding = true;
-//            explosion
-//        }
-//
-//    }
-//
-//
-//        /**
-//     * Get this Interactor's current occupying cells coordinates
-//     * @return (List of DiscreteCoordinates). May be empty but not null
-//     */
-//    @Override
-//    List<DiscreteCoordinates> getCurrentCells(){
-//
-//    }
-//
-//
-//    /**
-//     * Get this Interactor's current field of view cells coordinates
-//     * @return (List of DiscreteCoordinates). May be empty but not null
-//     */
-//    @Override
-//    List<DiscreteCoordinates> getFieldOfViewCells(){
-//
-//    }
-//
-//
-//    /**@return (boolean): true if this require cell interaction */
-//    @Override
-//    boolean wantsCellInteraction(){
-//
-//    }
-//    @Override
-//    /**@return (boolean): true if this require view interaction */
-//    boolean wantsViewInteraction(){
-//
-//    }
-//
-//    /**
-//     * Do this Interactor interact with the given Interactable
-//     * The interaction is implemented on the interactor side !
-//     * @param other (Interactable). Not null
-//     * @param isCellInteraction True if this is a cell interaction
-//     */
-//    @Override
-//    void interactWith(Interactable other, boolean isCellInteraction){
-//
-//    }
-//
-//        /**
-//     * Indicate if the current Interactable take the whole cell space or not
-//     * i.e. only one Interactable which takeCellSpace can be in a cell
-//     * (how many Interactable which don't takeCellSpace can also be in the same cell)
-//     * @return (boolean)
-//     */
-//    @Override
-//    public boolean takeCellSpace(){
-//        return false;
-//    }
-//
-//
-//    /**@return (boolean): true if this is able to have cell interactions*/
-//    @Override
-//    public boolean isCellInteractable(){
-//
-//    }
-//
-//    /**@return (boolean): true if this is able to have view interactions*/
-//    @Override
-//    boolean isViewInteractable(){
-//
-//    }
-//
-//    /** Call directly the interaction on this if accepted
-//     * @param v (AreaInteractionVisitor) : the visitor
-//     * */
-//    @Override
-//    void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction){
-//                ((ICoopInteractionVisitor) v).interactWith(this, isCellInteraction);
-//
-//    }
-//}
->>>>>>> 7b338a4bb9266b27d4472242aafed81ff8274fcb
